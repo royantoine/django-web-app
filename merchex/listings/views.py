1883f5854a9053f42876fa16f29815f0b6914e4c
royantoine <antoine.yor@gmail.com>
2022-01-20 18:08:36 +0100

    add models and database

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+# ~/projects/django-web-app/merchex/listings/views.py
 
-# Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render
+from listings.models import Band, Listing
+
+def hello(request):
+    bands = Band.objects.all()
+    return render(
+        request, 
+        'listings/hello.html',
+        {'bands': bands}
+    )
+
+def about(request):
+    return render(
+        request, 
+        'listings/about.html',
+    )
+
+def listings(request):
+    listings_ = Listing.objects.all()
+    return render(
+        request, 
+        'listings/listings.html',
+        {'listings_': listings_}
+    )
+
+def contact_us(request):
+    return render(
+        request, 
+        'listings/contact.html',
+    )
